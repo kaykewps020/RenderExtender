402d00097b5b246b7f2561c475b1325c79a6fa5f
kaykewps020 <kaykewps020@users.noreply.github.com>
2026-07-20 13:47:46 -0400

    Fix Minecraft.timer private access

--- src/main/java/me/cheat/client/mixin/MixinRenderLivingBase.java
+++ src/main/java/me/cheat/client/mixin/MixinRenderLivingBase.java
@@ -2,7 +2,6 @@ package me.cheat.client.mixin;
 
 import me.cheat.client.CheatClient;
 import me.cheat.client.events.RenderEvent;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +13,6 @@ public class MixinRenderLivingBase {
 
     @Inject(method = "doRender", at = @At("TAIL"))
     private void onDoRenderPost(CallbackInfo ci) {
-        float pt = Minecraft.getMinecraft().timer.renderPartialTicks;
-        CheatClient.EVENT_BUS.call(new RenderEvent(pt));
+        CheatClient.EVENT_BUS.call(new RenderEvent(0f));
     }
 }
